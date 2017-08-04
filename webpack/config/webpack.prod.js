3c7398d479398c28d77d83e80b2a4d3f1660bedf
VaishNathani <nathanivaishali@gmail.com>
2017-08-05 00:15:22 +0530

    prod config

--- webpack/config/webpack.prod.js
+++ webpack/config/webpack.prod.js
@@ -11,7 +11,7 @@ module.exports = webpackMerge(commonConfig, {
 
   output: {
     path: helpers.root('dist'),
-    publicPath: 'dashboard/',
+    publicPath: '/',
     filename: '[name].[hash].js',
     chunkFilename: '[id].[hash].chunk.js'
   },
