bed90036847ae44620a73f14934c2b805a8455c5
VaishNathani <nathanivaishali@gmail.com>
2017-08-04 17:23:45 +0530

    base path changes

--- webpack/config/webpack.prod.js
+++ webpack/config/webpack.prod.js
@@ -11,7 +11,7 @@ module.exports = webpackMerge(commonConfig, {
 
   output: {
     path: helpers.root('dist'),
-    publicPath: './dashboard/',
+    publicPath: '/dashboard/',
     filename: '[name].[hash].js',
     chunkFilename: '[id].[hash].chunk.js'
   },
